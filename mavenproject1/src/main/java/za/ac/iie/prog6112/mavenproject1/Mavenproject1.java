46301795799ca1843f4be902c9c2949f9724c54e
vincentjonas065 <vincentjonas065@gmail.com>
2026-08-24 18:12:00 +0200

    Initial commit

---
+++ mavenproject1/src/main/java/za/ac/iie/prog6112/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.ac.iie.prog6112.mavenproject1;
+
+/**
+ *
+ * @author Student
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
